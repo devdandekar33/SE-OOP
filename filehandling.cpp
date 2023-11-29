355bdd6939a76ac29c504a21955d2295881e7b6a
devdandekar33 <138193237+devdandekar33@users.noreply.github.com>
2023-11-29 19:22:01 +0530

    Update filehandling.cpp

--- filehandling.cpp
+++ filehandling.cpp
@@ -2,4 +2,48 @@
 #include<fstream>
 #include<string>
 using namespace std;
-cout<<"hello world"
+class student
+{
+public:
+string name, int rollno ;
+void input() {
+  cout<<"Enter the name" <<endl;
+  cin>>name;
+  cout<<"Enter the rollno. "<<endl;
+  cin>>rollno;
+  cout<<endl;
+}
+void display()
+{
+cout<<"Name is :"<<name<<endl;
+cout<<"Roll no. is :"<<rollno<<endl;
+}
+};
+int main()
+{
+  fstream file;
+  file.open("sample.txt",ios::out);
+  int n;
+  cout<<"Enter the number of students :";
+  cin>>n;
+  cout<<endl;
+  students S[n];
+  for(int i=1;i<=n;i++)
+{
+S[i].input;
+file<<"Name"<<S[i].name<<endl<<endl<<"Roll no. "<<S[i].rollno<<endl;
+}
+file.close();
+string line;
+file.open("sample.txt");
+while(!file.eof){
+file>>line;
+cout<<line;
+}
+return 0;
+}
+
+
+
+
+  
