39174f4fe84c2575e1dc58192db297c434e48f5b
devdandekar33 <138193237+devdandekar33@users.noreply.github.com>
2023-12-12 15:37:55 +0530

    Update filehandling.cpp

--- filehandling.cpp
+++ filehandling.cpp
@@ -1,49 +1,60 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-class student
-{
-public:
-string name, int rollno ;
-void input() {
-  cout<<"Enter the name" <<endl;
-  cin>>name;
-  cout<<"Enter the rollno. "<<endl;
-  cin>>rollno;
-  cout<<endl;
-}
-void display()
-{
-cout<<"Name is :"<<name<<endl;
-cout<<"Roll no. is :"<<rollno<<endl;
-}
+class Student
+    {
+    public:
+    string name; int rno;
+
+    public:
+    void input()
+    {
+    cout<<endl<<"Enter Name: ";
+    cin>>name;
+    cout<<"Enter Roll Number: ";
+    cin>>rno;
+    
+    cout<<endl;
+    }
+
+    void display()
+    {
+    cout<<"Name: "<<name<<endl;
+    cout<<"Roll Number: "<<rno<<endl;
+    }
 };
+
 int main()
 {
-  fstream file;
-  file.open("sample.txt",ios::out);
-  int n;
-  cout<<"Enter the number of students :";
-  cin>>n;
-  cout<<endl;
-  students S[n];
-  for(int i=1;i<=n;i++)
-{
-S[i].input;
-file<<"Name"<<S[i].name<<endl<<endl<<"Roll no. "<<S[i].rollno<<endl;
+    fstream file;
+    file.open("sample.txt",ios::out);
+    int n;
+    cout<<"Enter Number of students: ";
+    cin>>n;
+    cout<<endl;
+    Student s[n];
+    for(int i=0;i<n;i++)
+    {
+    s[i].input();
+    file<<"Name: "<<s[i].name<<endl<<"Roll Number: "<<s[i].rno<<endl;
+    }
+    file.close();
+    string line;
+    file.open("sample.txt",ios::in);
+    do{
+    file>>line;
+    cout<<line<<" ";
+    }
+    while(!file.eof());
+    cout<<"After clearing file"<<endl;
+    file.open("sample.txt",std::ios::trunc);
+    do{
+    file>>line;
+    cout<<line<<" ";
+    }
+    while(!file.eof());
+    cout<<"Thank you"<<endl;
+    return 0;
 }
-file.close();
-string line;
-file.open("sample.txt");
-while(!file.eof){
-file>>line;
-cout<<line;
-}
-return 0;
-}
 
-
-
-
-  
